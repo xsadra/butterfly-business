51bc0a6e265c06d51f12e906cc3f15d6f26f93b9
LeenRaslan <leen.b.r.357@gmail.com>
2019-02-16 19:01:25 +0100

    Validator

---
+++ src/creditcard/Validator.java
@@ -0,0 +1,19 @@
+package creditcard;
+
+import java.util.List;
+
+public abstract class Validator {
+
+    private List<String> possibilities;
+
+    public Validator(List<String> possibilities) {
+        this.possibilities = possibilities;
+    }
+
+    protected List<String> getPossibilities() {
+        return possibilities;
+    }
+
+    public abstract boolean isValid(String cardNumber);
+
+}
